f112ac7703bf7dd62cb78457ee53ca59b846c7d1
Tsekko <maxime@my-jugaad.eu>
2022-08-03 10:12:30 +0200

    First commit

---
+++ lib/features/movie_flow/movie_flow.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:movie_recommandation_app/features/movie_flow/genre/genre_screen.dart';
+import 'package:movie_recommandation_app/features/movie_flow/landing/landing_screen.dart';
+
+class MovieFlow extends StatefulWidget {
+  const MovieFlow({Key? key}) : super(key: key);
+
+  @override
+  _MovieFlowState createState() => _MovieFlowState();
+}
+
+class _MovieFlowState extends State<MovieFlow> {
+  final pageController = PageController();
+
+  void nextPage() {
+    pageController.nextPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
+  void previousPage() {
+    pageController.previousPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        LandingScreen(nextPage: nextPage, previousPage: previousPage),
+        GenreScreen(nextPage: nextPage, previousPage: previousPage),
+        Scaffold(body: Container(color: Colors.blue)),
+        Scaffold(body: Container(color: Colors.yellow)),
+      ],
+    );
+  }
+}
